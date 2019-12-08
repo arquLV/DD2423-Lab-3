14da8d0a42f809be3cce48b4e7a5080fa0939237
Arturs Kurzemnieks <arturs.kurzemnieks@gmail.com>
2019-12-09 00:02:45 +0100

    Q3

--- kmeans_segm.m
+++ kmeans_segm.m
@@ -2,12 +2,15 @@ function [segmentation, centers] = kmeans_segm(image, K, L, seed)
 % image : (H,W,3)
 [H,W,~] = size(image);
 
-pixels = reshape(image, W*H, 3);
+pixels = double(reshape(image, W*H, 3));
 % disp(size(pixels));
 
-rng(seed); % seed the random generator
+if nargin > 3
+    rng(seed); % seed the random generator
+end
+
 %centers = randi([0,255], K, 3); % K random RGB centers
-centers = pixels(randperm(W*H,K),:);
+centers = double(pixels(randperm(W*H,K),:));
 
 for i = 1:L
     distances = pdist2(pixels, centers); % (W*H, K)
